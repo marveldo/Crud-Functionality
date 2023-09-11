ea7680cc47a5988dff4934098afe66f03dadedc0
marveldo <109154909+marveldo@users.noreply.github.com>
2023-09-11 20:11:00 +0100

    This is the testing file for this api this is the python file that tested the api

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import requests
 name1 = 'Marvelous'
 name2 = 'John'
 
+# testing Create
 url = f'https://crud-functionality-hqud.onrender.com/api'
 data = {'name': name1}
 
@@ -11,7 +12,7 @@ data = response.json()
 print(f"Your data has been created it is {data}")
 
 
-
+# function to test GET request
 def get(id):
     url3 = f'https://crud-functionality-hqud.onrender.com/api/{id}/'
     response = requests.get(url3)
@@ -20,6 +21,7 @@ def get(id):
 
 get(int(data['id']))
 
+# function to test PUT request
 def update(id,name):
     url5 = f'https://crud-functionality-hqud.onrender.com/api/{id}/'
     data = {'name': name}
@@ -30,6 +32,7 @@ def update(id,name):
 
 update(int(data['id']), name2)
 
+# function to test DELETE request
 def delete(id):
     url4 = f'https://crud-functionality-hqud.onrender.com/api/{id}/'
 
