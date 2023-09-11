9d67aa2bb12a522c529b72e7d7da2a0037ce2e9b
marveldo <109154909+marveldo@users.noreply.github.com>
2023-09-12 00:38:35 +0100

    added new files

--- crud/urls.py
+++ crud/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import home
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
     path('api', include('api.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
 
